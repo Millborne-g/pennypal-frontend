5b7a9fcac308a3ce36b99792b031df03d6d6f5cc
Millborne Galamiton <150405245+Millborne@users.noreply.github.com>
2024-09-19 10:05:08 +0800

    Feature/add date (#4)
    * first commit
    
    * Add date field

--- src/components/popups/UpsertBalanceExpensesModal.tsx
+++ src/components/popups/UpsertBalanceExpensesModal.tsx
@@ -14,10 +14,12 @@ import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
 import LoadingButton from "@mui/lab/LoadingButton";
 
-// import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+// date picker
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
 
 // components
 import { ErrorSnackbar } from "../ErrorSnackbar";
@@ -39,7 +41,7 @@ export const style = {
     boxShadow: 24,
     p: 4,
     maxHeight: "90vh",
-    overflow: "auto"
+    overflow: "auto",
 };
 
 export const UpsertBalanceExpensesModal = ({
@@ -67,6 +69,8 @@ export const UpsertBalanceExpensesModal = ({
     const [openErrorSnackBar, setOpenErrorSnackbar] = useState(false);
     const [snackbarText, setSnackbarText] = useState("");
 
+    const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
+
     const [load, setLoad] = useState<boolean>(false);
 
     const handleChange = (event: SelectChangeEvent) => {
@@ -75,15 +79,17 @@ export const UpsertBalanceExpensesModal = ({
 
     const submitAmount = async (e: any) => {
         e.preventDefault();
-        if (category !== "" && amount) {
+        if (category !== "" && amount && selectedDate) {
             const newData = {
                 category: category,
                 amount: amount,
                 user: userDetails._id,
                 ...(note !== "" && { note: note }),
+                date: new Date(selectedDate.toISOString()),
             };
             // console.log("aaaaaaaaaaaah");
             setLoad(true);
+
             await addFunction(newData);
             refetch && refetch();
             setOpenModal(false);
@@ -93,6 +99,8 @@ export const UpsertBalanceExpensesModal = ({
                 setSnackbarText("Please enter an amount.");
             } else if (!category) {
                 setSnackbarText("Choose a category.");
+            } else if (!selectedDate) {
+                setSnackbarText("Please enter the date.");
             }
         }
     };
@@ -121,7 +129,7 @@ export const UpsertBalanceExpensesModal = ({
                     <form onSubmit={(e) => submitAmount(e)}>
                         <Stack gap={2}>
                             {addBalExText === "Expense" ? (
-                                <FormControl fullWidth >
+                                <FormControl fullWidth>
                                     <InputLabel>Category</InputLabel>
                                     <Select
                                         value={category}
@@ -172,19 +180,25 @@ export const UpsertBalanceExpensesModal = ({
                                     setNote(e.target.value);
                                 }}
                             />
-                            {/* <TextareaAutosize/> */}
 
-                            {/* <Stack>
+                            <Stack>
                                 <LocalizationProvider
                                     dateAdapter={AdapterDayjs}
                                 >
-                                    <DemoContainer components={["DatePicker"]} sx={{
-                                        padding: 0
-                                    }}>
-                                        <DatePicker label="Basic date picker" />
+                                    <DemoContainer
+                                        components={["DatePicker"]}
+                                        sx={{ mt: -1 }}
+                                    >
+                                        <DatePicker
+                                            label="Basic date picker"
+                                            value={selectedDate}
+                                            onChange={(newValue) =>
+                                                setSelectedDate(newValue)
+                                            }
+                                        />
                                     </DemoContainer>
                                 </LocalizationProvider>
-                            </Stack> */}
+                            </Stack>
 
                             <TextField
                                 label="Enter the Amount"
